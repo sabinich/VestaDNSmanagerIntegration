90778117c7b02e86eb3914b34b15eaacca7df57e
vadim s. sabinich <1430016+sabinich@users.noreply.github.com>
2020-06-24 16:51:13 +0800

    Create add-api.php

---
+++ web/dnsmanager/add-api.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @author Ilya Zelenin <wyster@make.im>
+ */
+$var1 = $argv[1];
+
+require __DIR__ . '/functions.php';
+$configDnsManager = dnsManagerGetConfig();
+if ($configDnsManager['enabled'] && count($configDnsManager['dns']) > 0) {
+    foreach ($configDnsManager['dns'] as $master) {
+        $master = array_map(function ($value) {
+            return trim($value);
+        }, explode(',', $master));
+        list($url, $login, $password) = $master;
+
+        $dnsManagerParams = array(
+            'sok' => 'yes',
+            'out' => 'text',
+            'authinfo' => $login . ':' . $password,
+            'name' => $v_domain,
+            'master' => $configDnsManager['master'],
+            'func' => 'domain.edit'
+        );
+
+        try {
+            $dnsManagerResponse = dnsManagerSendRequest(dnsManagerBuildUrl($url), $dnsManagerParams);
+        } catch (Exception $e) {
+        }
+    }
+}
