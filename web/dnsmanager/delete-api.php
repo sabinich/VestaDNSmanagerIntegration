e08523cd2646ff4005f4c65391d1d7bbf3e73f44
vadim s. sabinich <1430016+sabinich@users.noreply.github.com>
2020-06-24 16:52:03 +0800

    Create delete-api.php

---
+++ web/dnsmanager/delete-api.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @author Ilya Zelenin <wyster@make.im>
+ */
+$var1 = $argv[1];
+
+require __DIR__ . '/functions.php';
+$configDnsManager = dnsManagerGetConfig();
+if ($configDnsManager['enabled'] && count($configDnsManager['dns']) > 0) {
+    foreach ($configDnsManager['dns'] as $master) {
+        $master = array_map(function ($value) {
+            return trim($value);
+        }, explode(',', $master));
+        list($url, $login, $password) = $master;
+
+        $dnsManagerParams = array(
+            'out' => 'text',
+            'authinfo' => $login . ':' . $password,
+            'elid' => $v_domain,
+            'func' => 'domain.delete'
+        );
+
+        try {
+            $dnsManagerResponse = dnsManagerSendRequest(dnsManagerBuildUrl($url), $dnsManagerParams);
+        } catch (Exception $e) {
+        }
+    }
+}
